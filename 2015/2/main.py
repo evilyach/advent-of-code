60da3a4b444983af3029754ea8cccfb941328bbf
Ilya Chesalin <ilya.chesalin@gmail.com>
2022-01-04 17:24:44 +0300

    Refactor day 2 of Advent of Code 2015

--- 2015/2/main.py
+++ 2015/2/main.py
@@ -3,19 +3,17 @@ class IWasToldThereWouldBeNoMath:
         with open("2015/2/input.txt", "r") as f:
             self.sizes = [sorted([int(i) for i in line.strip().split("x")]) for line in f.readlines()]
 
-    def find_total_paper_area(self):
+    def find_total(self, count):
         sum = 0
         for package in self.sizes:
-            x, y, z = package
-            sum += 3*x*y + 2*y*z + 2*x*z
+            sum += count(*package)
         return sum
 
+    def find_total_paper_area(self):
+        return self.find_total(lambda x, y, z: 3*x*y + 2*y*z + 2*x*z)
+
     def find_total_ribbon_length(self):
-        sum = 0
-        for package in self.sizes:
-            x, y, z = package
-            sum += 2*x + 2*y + x*y*z
-        return sum
+        return self.find_total(lambda x, y, z: 2*x + 2*y + x*y*z)
 
 if __name__ == "__main__":
     task = IWasToldThereWouldBeNoMath()
